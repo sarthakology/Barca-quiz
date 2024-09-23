5b6e6337895e0ec67a889623bafd6f3c5ac1402b
sarthak <thesarthakchauhan@gmail.com>
2024-09-23 12:58:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-5xl font-extrabold text-white shadow-lg p-6 rounded-lg bg-opacity-60 bg-black transform transition duration-500 hover:scale-110">
+        Hello world!
+      </h1>
     </div>
   );
 }
